b9fa9a58ac268954d0dd0d968f17287efcc968a8
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2001-10-01 13:00:45 +0000

    no message

--- kernel/os/rdfstab.asm
+++ kernel/os/rdfstab.asm
@@ -46,84 +46,544 @@ INCLUDE rdfs.inc
 code	SEGMENT byte public use16 'CODE'
 
 	assume cs:code
-;
-; Table is generated using 1.1777758960 as factor
-;
 
-	public ExtentSizeTab
-
-ExtentSizeTab:
-es00	DD 00000001h, 00000001h, 00000001h, 00000001h
-es04	DD 00000001h, 00000002h, 00000002h, 00000003h
-es08	DD 00000003h, 00000004h, 00000005h, 00000006h
-es0C	DD 00000007h, 00000008h, 00000009h, 0000000Bh
-es10	DD 0000000Dh, 00000010h, 00000013h, 00000016h
-es14	DD 0000001Ah, 0000001Fh, 00000024h, 0000002Bh
-es18	DD 00000032h, 0000003Bh, 00000046h, 00000052h
-es1C	DD 00000061h, 00000073h, 00000087h, 0000009Fh
-es20	DD 000000BBh, 000000DDh, 00000104h, 00000133h
-es24	DD 00000169h, 000001A9h, 000001F5h, 0000024Eh
-es28	DD 000002B7h, 00000333h, 000003C5h, 00000470h
-es2C	DD 0000053Ah, 00000628h, 00000741h, 0000088Bh
-es30	DD 00000A10h, 00000BDAh, 00000DF5h, 00001071h
-es34	DD 0000135Dh, 000016CEh, 00001ADCh, 00001FA3h
-es38	DD 00002543h, 00002BE2h, 000033B0h, 00003CE0h
-es3C	DD 000047B3h, 00005472h, 00006375h, 00007524h
-es40	DD 000089F7h, 0000A27Eh, 0000BF61h, 0000E166h
-es44	DD 00010979h, 000138ABh, 00017040h, 0001B1B8h
-es48	DD 0001FED3h, 000259A3h, 0002C497h, 00034290h
-es4C	DD 0003D6EDh, 000485ABh, 00055379h, 000645DEh
-es50	DD 0007635Ah, 0008B397h, 000A3F9Ah, 000C1204h
-es54	DD 000E3759h, 0010BE56h, 0013B858h, 001739D1h
-es58	DD 001B5AD7h, 002037C7h, 0025F208h, 002CB0F4h
-es5C	DD 0034A2E1h, 003DFE63h, 004903C3h, 0055FEB5h
-es60	DD 00654864h, 007749D5h, 008C7EB9h, 00A578BEh
-es64	DD 00C2E376h, 00E588F6h, 010E573Eh, 013E669Fh
-es68	DD 0177013Fh, 01B9ABF4h, 020830B6h, 0264AAE6h
-es6C	DD 02D195C9h, 0351DD94h, 03E8F37Eh, 049AE569h
-es70	DD 056C79B7h, 0663501Ch, 0786083Ah, 08DC6F2Ah
-es74	DD 0A6FB509h, 0C4AAC1Ch, 0E7A1325h, 110CECF1h
-es78	DD 1414E773h, 17A6D53Ch, 1BDB3C7Eh, 20CEFF8Bh
-es7C	DD 26A4233Dh, 2D82B8A6h
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; This table is the sum of the ExtentSizeTab
-; The total sum is 2^23
 ;
+;		NAME:			FormatInfoSector
+;
+;		DESCRIPTION:	Format info sector
+;
+;		PARAMETERS:		DS			Drive
+;						ECX			Number of sectors
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-ExtentPosTab:
-ep00	DD 00000000h, 00000001h, 00000002h, 00000003h
-ep04	DD 00000004h, 00000005h, 00000007h, 00000009h
-ep08	DD 0000000Ch, 0000000Fh, 00000013h, 00000018h
-ep0C	DD 0000001Eh, 00000025h, 0000002Dh, 00000036h
-ep10	DD 00000041h, 0000004Eh, 0000005Eh, 00000071h
-ep14	DD 00000087h, 000000A1h, 000000C0h, 000000E4h
-ep18	DD 0000010Fh, 00000141h, 0000017Ch, 000001C2h
-ep1C	DD 00000214h, 00000275h, 000002E8h, 0000036Fh
-ep20	DD 0000040Eh, 000004C9h, 000005A6h, 000006AAh
-ep24	DD 000007DDh, 00000946h, 00000AEFh, 00000CE4h
-ep28	DD 00000F32h, 000011E9h, 0000151Ch, 000018E1h
-ep2C	DD 00001D51h, 0000228Bh, 000028B3h, 00002FF4h
-ep30	DD 0000387Fh, 0000428Fh, 00004E69h, 00005C5Eh
-ep34	DD 00006CCFh, 0000802Ch, 000096FAh, 0000B1D6h
-ep38	DD 0000D179h, 0000F6BCh, 0001229Eh, 0001564Eh
-ep3C	DD 0001932Eh, 0001DAE1h, 00022F53h, 000292C8h
-ep40	DD 000307ECh, 000391E3h, 00043461h, 0004F3C2h
-ep44	DD 0005D528h, 0006DEA1h, 0008174Ch, 0009878Ch
-ep48	DD 000B3944h, 000D3817h, 000F91BAh, 00125651h
-ep4C	DD 001598E1h, 00196FCEh, 001DF579h, 002348F2h
-ep50	DD 00298ED0h, 0030F22Ah, 0039A5C1h, 0043E55Bh
-ep54	DD 004FF75Fh, 005E2EB8h, 006EED0Eh, 0082A566h
-ep58	DD 0099DF37h, 00B53A0Eh, 00D571D5h, 00FB63DDh
-ep5C	DD 012814D1h, 015CB7B2h, 019AB615h, 01E3B9D8h
-ep60	DD 0239B88Dh, 029F00F1h, 03164AC6h, 03A2C97Fh
-ep64	DD 0448423Dh, 050B25B3h, 05F0AEA9h, 06FF05E7h
-ep68	DD 083D6C86h, 09B46DC5h, 0B6E19B9h, 0D764A6Fh
-ep6C	DD 0FDAF555h, 12AC8B1Eh, 15FE68B2h, 19E75C30h
-ep70	DD 1E824199h, 23EEBB50h, 2A520B6Ch, 31D813A6h
-ep74	DD 3AB482D0h, 452437D9h, 516EE3F5h, 5FE8F71Ah
-ep78	DD 70F5E40Bh, 850ACB7Eh, 9CB1A0BAh, 0B88CDD38h
-ep7C	DD 0D95BDCC3h, 0FFFFFFFFh
+	public FormatInfoSector
+
+FormatInfoSector	Proc near
+	pushad
+;
+	mov al,ds:drive_nr
+	mov edx,1
+	NewSector
+;
+	mov edi,esi
+	mov ecx,128
+	xor eax,eax
+	rep stos dword ptr es:[edi]
+;
+	mov edi,esi
+	mov esi,OFFSET info_sector
+	mov ecx,SIZE rdfs_info_struc
+	rep movs byte ptr es:[edi],[esi]
+;
+	ModifySector
+	UnlockSector
+;
+	popad
+	ret
+FormatInfoSector	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			FormatAllocationArr
+;
+;		DESCRIPTION:	Format allocation array
+;
+;		PARAMETERS:		DS			Drive
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	public FormatAllocationArr
+
+FormatAllocationArr	Proc near
+	pushad
+;
+	mov al,ds:drive_nr
+	mov edx,2
+	NewSector
+;
+	mov eax,-1
+	mov ecx,ds:info_sector.ri_root_dir
+format_alloc_used_loop:
+	mov dword ptr es:[esi],eax
+	add esi,4
+	test si,01FFh
+	jnz format_alloc_used_next
+;
+	ModifySector
+	UnlockSector
+	inc edx
+	push ax
+	mov al,ds:drive_nr
+	NewSector
+	pop ax
+
+format_alloc_used_next:
+	loop format_alloc_used_loop
+;
+	xor eax,eax
+
+format_alloc_free_loop:
+	mov dword ptr es:[esi],eax
+	add esi,4
+	test si,01FFh
+	jnz format_alloc_free_loop
+;
+	ModifySector
+	UnlockSector
+	inc edx
+	cmp edx,ds:info_sector.ri_root_dir
+	je format_alloc_done
+;
+	push ax
+	mov al,ds:drive_nr
+	NewSector
+	pop ax
+	jmp format_alloc_free_loop
+
+format_alloc_done:
+	popad
+	ret
+FormatAllocationArr	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			WriteInfoSector
+;
+;		DESCRIPTION:	Write info sector
+;
+;		PARAMETERS:		DS			Drive
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+WriteInfoSector	Proc near
+	pushad
+;
+	mov al,ds:drive_nr
+	mov edx,1
+	LockSector
+;
+	mov edi,esi
+	mov esi,OFFSET info_sector
+	mov ecx,SIZE rdfs_info_struc
+	rep movs byte ptr es:[edi],[esi]
+;
+	ModifySector
+	UnlockSector
+;
+	popad
+	ret
+WriteInfoSector	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			FreeAllocationArr
+;
+;		DESCRIPTION:	Free allocation array
+;
+;		PARAMETERS:		DS			Drive
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	public FreeAllocationArr
+
+FreeAllocationArr	Proc near
+	push es
+	push fs
+	push ecx
+	push edx
+	push edi
+;
+	mov cx,ds:alloc_sel
+	or cx,cx
+	jz free_alloc_done
+;
+	mov fs,cx
+	mov ecx,fs:as_sectors
+	mov edi,SIZE alloc_struc
+
+free_alloc_unlock_loop:
+	mov ebx,fs:[edi]
+	or ebx,ebx
+	jz free_alloc_unlock_next
+;
+	UnlockSector
+
+free_alloc_unlock_next:
+	add edi,4
+	sub ecx,80h
+	ja free_alloc_unlock_loop
+;
+	mov edx,fs:as_base
+	mov ecx,fs:as_size
+	FreeLinear
+	mov ax,fs
+	mov es,ax
+	xor ax,ax
+	mov fs,ax
+	FreeMem
+	mov ds:alloc_sel,0
+
+free_alloc_done:
+	pop edi
+	pop edx
+	pop ecx
+	pop fs
+	pop es
+	ret
+FreeAllocationArr	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			ReadAllocSectors
+;
+;		DESCRIPTION:	Req & wait for allocate sectors
+;
+;		PARAMETERS:		DS			Drive
+;						FS			Alloc segment
+;						ESI			Alloc buffer
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ReadAllocSectors	Proc near
+	mov edi,SIZE alloc_struc
+	mov edx,ds:info_sector.ri_hole_start
+	shr edx,7
+	add edx,ds:info_sector.ri_free_arr
+	mov ecx,fs:as_sectors
+
+read_alloc_req_loop:
+	mov al,ds:drive_nr
+	ReqSector
+	mov fs:[edi],ebx
+	add edi,4
+	add esi,200h
+	inc edx
+	sub ecx,80h
+	ja read_alloc_req_loop
+;
+	mov ecx,fs:as_sectors
+
+read_alloc_wait_loop:
+	sub esi,200h
+	sub edi,4
+	mov ebx,fs:[edi]
+	WaitForSector
+	jc read_alloc_fail
+;
+	sub ecx,80h
+	ja read_alloc_wait_loop
+;
+	clc
+	jmp read_alloc_done
+
+read_alloc_fail:
+	lea eax,[edi + 4 - SIZE alloc_struc]
+	shl eax,5
+	mov edx,ds:info_sector.ri_hole_start
+	and dl,80h
+	add edx,eax
+	sub edx,ds:info_sector.ri_hole_start
+	add ds:info_sector.ri_hole_start,edx
+	sub ds:info_sector.ri_hole_size,edx
+	sub ds:info_sector.ri_free_sectors,edx	
+	stc
+
+read_alloc_done:
+	ret
+ReadAllocSectors	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			GetAllocArr
+;
+;		DESCRIPTION:	Get allocation array
+;
+;		PARAMETERS:		DS			Drive
+;						ECX			Number of sectors wanted
+;
+;		RETURNS:		FS			Alloc segment
+;						ESI			Alloc buffer
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+GetAllocArr	Proc near
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push edi
+;
+	mov dx,ds:alloc_sel
+	or dx,dx
+	jz get_alloc_new
+;
+	mov fs,dx
+	mov edx,ds:info_sector.ri_hole_start
+	sub edx,fs:as_start
+	jc get_alloc_realloc
+;
+	add edx,ecx
+	cmp edx,fs:as_sectors
+	ja get_alloc_realloc
+;
+	mov esi,fs:as_base
+	clc
+	jmp get_alloc_ok
+
+get_alloc_realloc:
+	xor ax,ax
+	mov fs,ax
+	call FreeAllocationArr
+
+get_alloc_new:
+	mov edx,ds:info_sector.ri_hole_start
+	mov eax,edx
+	and dl,80h
+	and eax,7Fh
+	add eax,ecx
+	dec eax
+	shr eax,7
+	and al,0F8h
+	add eax,8
+	mov ecx,eax
+	shl eax,2
+	add eax,SIZE alloc_struc
+	push es
+	AllocateSmallGlobalMem
+	push ecx
+	mov edi,SIZE alloc_struc
+	xor eax,eax
+	rep stos dword ptr es:[edi]
+	pop ecx
+	mov bx,es
+	mov ds:alloc_sel,bx
+	mov fs,bx
+	pop es
+	mov fs:as_start,edx
+	shl ecx,7
+	mov fs:as_sectors,ecx
+;
+	mov eax,ecx
+	shl eax,2
+	AllocateBigLinear	
+	mov esi,edx
+	mov fs:as_base,esi
+	mov fs:as_size,eax
+	call ReadAllocSectors
+
+get_alloc_ok:
+	pop edi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+GetAllocArr	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			MarkAllocSectors
+;
+;		DESCRIPTION:	Markup allocate sectors
+;
+;		PARAMETERS:		DS			Drive
+;						FS			Alloc segment
+;						ESI			Alloc buffer
+;						ECX			Sectors to mark
+;						EBP			Markup value
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+MarkAllocSectors	Proc near
+	push eax
+	push ebx
+	push ecx
+	push esi
+	push edi
+;
+	mov eax,ds:info_sector.ri_hole_start
+	shl eax,2
+	and eax,1FCh
+	add esi,eax	
+	mov edi,SIZE alloc_struc
+	xor ebx,ebx
+
+mark_alloc_loop:
+	mov eax,es:[esi]
+	or eax,eax	
+	jnz mark_alloc_fail
+;
+	inc ebx
+	mov es:[esi],ebp
+	add esi,4
+	sub ecx,1
+	jnz mark_alloc_loop
+;
+	clc
+	jmp mark_alloc_done
+
+mark_alloc_fail:
+	or ebx,ebx
+	jz mark_alloc_advance
+;
+	push ebx
+	push esi
+	xor eax,eax
+
+mark_alloc_revert:
+	sub esi,4
+	mov es:[esi],eax
+	sub ebx,1
+	jnz mark_alloc_revert
+;
+	pop esi
+	pop ebx
+
+mark_alloc_advance:
+	inc ebx
+	add esi,4
+	test si,1FFh
+	jz mark_alloc_fail_done
+;
+	mov eax,es:[esi]
+	or eax,eax	
+	jnz mark_alloc_advance
+
+mark_alloc_fail_done:
+	add ds:info_sector.ri_hole_start,ebx
+	sub ds:info_sector.ri_hole_size,ebx
+	sub ds:info_sector.ri_free_sectors,ebx	
+	stc
+
+mark_alloc_done:
+	pop edi
+	pop esi
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+MarkAllocSectors	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			WriteAllocSectors
+;
+;		DESCRIPTION:	Write allocate sectors
+;
+;		PARAMETERS:		DS			Drive
+;						FS			Alloc segment
+;						ECX			Sectors to write
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+WriteAllocSectors	Proc near
+	push eax
+	push ebx
+	push ecx
+	push edi
+;
+	mov eax,ds:info_sector.ri_hole_start
+	sub eax,fs:as_start
+	shr eax,7
+	lea edi,[4*eax + SIZE alloc_struc]
+;
+	add ecx,ds:info_sector.ri_hole_start
+	dec ecx
+	sub ecx,fs:as_start
+	shr ecx,7
+	sub ecx,eax
+	inc ecx	
+
+write_alloc_loop:
+	mov ebx,fs:[edi]
+	ModifySeqSector
+	add edi,4
+	sub ecx,1
+	jnz write_alloc_loop
+;
+	pop edi
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+WriteAllocSectors	Endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;		NAME:			AllocateSectors
+;
+;		DESCRIPTION:	Allocate sectors
+;
+;		PARAMETERS:		DS			Drive
+;						ECX			Number of sectors
+;						EAX			Control sector
+;
+;		RETURNS:		EDX			Sector #
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	public AllocateSectors
+
+AllocateSectors	Proc near
+	push es
+	push fs
+	push eax
+	push ebx
+	push ecx
+	push esi
+	push edi
+	push ebp
+;
+	mov ebp,eax
+	EnterSection ds:alloc_section
+
+alloc_retry:
+	cmp ecx,ds:info_sector.ri_hole_size
+	ja alloc_sectors_fail
+;
+	call GetAllocArr
+	jc alloc_retry
+;
+	call MarkAllocSectors
+	jc alloc_retry
+;
+	call WriteAllocSectors
+;
+	mov edx,ds:info_sector.ri_hole_start
+	add ds:info_sector.ri_hole_start,ecx
+	sub ds:info_sector.ri_hole_size,ecx
+	sub ds:info_sector.ri_free_sectors,ecx
+	clc
+	jmp alloc_sectors_done
+
+alloc_sectors_fail:
+	stc
+
+alloc_sectors_done:
+	call WriteInfoSector
+	LeaveSection ds:alloc_section
+;
+	pop ebp
+	pop edi
+	pop esi
+	pop ecx
+	pop ebx
+	pop eax
+	pop fs
+	pop es
+	ret
+AllocateSectors	Endp
 
 code	ENDS
 
