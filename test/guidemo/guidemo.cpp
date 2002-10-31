8093d538b5889a7aa1ca05dc298e9af3adbb85fa
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2002-10-31 08:01:57 +0000

    no message

--- test/guidemo/guidemo.cpp
+++ test/guidemo/guidemo.cpp
@@ -18,10 +18,10 @@ void RandomLine()
 	int x1, y1;
 	int x2, y2;
 
-	x1 = random(width);
-	y1 = random(height);
-	x2 = random(width);
-	y2 = random(height);
+	x1 = random(width + 200) - 100;
+	y1 = random(height + 200) - 100;
+	x2 = random(width + 200) - 100;
+	y2 = random(height + 200) - 100;
 	RdosSetDrawColor(VbeHandle, mkcolor(random(256), random(256), random(256)));
 	RdosSetLGOP(VbeHandle, random(11));
 	RdosDrawLine(VbeHandle, x1, y1, x2, y2);
@@ -38,10 +38,12 @@ void RandomRect()
 		RdosSetHollowStyle(VbeHandle);
 	else
 		RdosSetFilledStyle(VbeHandle);
-	x = random(width);
-	y = random(height);
-	w = random(width - x);
-	h = random(height - y);
+	x = random(width + 200) - 100;
+	y = random(height + 200) - 100;
+	w = random(width + 100);
+	h = random(height + 100);
+	x -= w / 2;
+	y -= h / 2;
 	RdosDrawRect(VbeHandle, x, y, w, h);
 }
 
@@ -50,10 +52,12 @@ void RandomEllipse()
 	int x, y;
 	int w, h;
 
-	x = random(width);
-	y = random(height);
-	w = random(width - x);
-	h = random(height - y);
+	x = random(width + 200) - 100;
+	y = random(height + 200) - 100;
+	w = random(width + 100);
+	h = random(height + 100);
+	x -= w / 2;
+	y -= h / 2;
 	RdosSetDrawColor(VbeHandle, mkcolor(random(256), random(256), random(256)));
 	RdosSetLGOP(VbeHandle, random(11));
 	if (random(2) == 0)
@@ -71,8 +75,10 @@ void RandomText()
 
 	sprintf(str, "%d", count);
 	RdosGetStringMetrics(font, str, &w, &h);
-	x = random(width - w);
-	y = random(height - h);
+	x = random(width + 200) - 100;
+	y = random(height + 200) - 100;
+	x -= w / 2;
+	y -= h / 2;
 	RdosSetDrawColor(VbeHandle, mkcolor(random(256), random(256), random(256)));
 	RdosSetLGOP(VbeHandle, random(11));
 	RdosDrawString(VbeHandle, x, y, str);
@@ -80,12 +86,35 @@ void RandomText()
 
 void cdecl main()
 {
+	int i;
+
 	RdosWaitMilli(250);
 	bpp = 32;
 	width = 800;
 	height = 600;
 	VbeHandle = RdosSetVBEMode(&bpp, &width, &height, &rowsize, &buf);
-	RdosSetClipRect(VbeHandle, 100, 100, width - 100, height - 100);
+//	RdosSetClipRect(VbeHandle, 100, 100, width - 100, height - 100);
+
+	RdosSetLGOP(VbeHandle, LGOP_ADD);
+
+	RdosSetDrawColor(VbeHandle, mkcolor(0, 0, 255));
+	for (i = 0; i < 800; i++)
+		RdosDrawLine(VbeHandle, 0, 3 * i, 3 * i, 0);
+
+	RdosWaitMilli(5000);
+
+	RdosSetDrawColor(VbeHandle, mkcolor(0, 255, 0));
+	for (i = -250; i < 250; i++)
+		RdosDrawLine(VbeHandle, height, width - 3 * i, 3 * i, 0);
+
+	RdosWaitMilli(5000);
+
+	RdosSetDrawColor(VbeHandle, mkcolor(0, 255, 255));
+	for (i = -250; i < 250; i++)
+		RdosDrawLine(VbeHandle, 0, 3 * i, height - 3 * i, width);
+
+	RdosWaitMilli(5000);
+
 	font = RdosOpenFont(60);
 	RdosSetFont(VbeHandle, font);
 	for (;;)
