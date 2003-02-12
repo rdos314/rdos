44c60e48a544fc640c2cffd6ad8723c4bc140652
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2003-02-12 12:29:37 +0000

    no message

---
+++ kernel/os/wait.asm
@@ -0,0 +1,113 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; RDOS operating system
+; Copyright (C) 1988-2000, Leif Ekblad
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version. The only exception to this rule
+; is for commercial usage in embedded systems. For information on
+; usage in commercial embedded systems, contact embedded@rdos.net
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;
+; The author of this program may be contacted at leif@rdos.net
+;
+; WAIT.ASM
+; Abortable single & multiple wait functionality
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+						
+		NAME timer
+
+GateSize = 16
+
+INCLUDE protseg.def
+INCLUDE system.def
+INCLUDE driver.def
+INCLUDE user.def
+INCLUDE os.def
+INCLUDE system.inc
+INCLUDE user.inc
+INCLUDE os.inc
+
+
+wait_thread_seg   STRUC
+
+wp_dummy    DB ?
+
+wait_thread_seg   ENDS
+
+wait_seg    STRUC
+
+w_dummy     DB ?
+
+wait_seg    ENDS
+
+	.386p
+
+code	SEGMENT byte public use16 'CODE'
+
+	assume cs:code
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;	
+;
+;		NAME:			init_wait_thread
+;
+;		DESCRIPTION:	Init per-thread data
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+init_wait_thread	PROC far
+    push ds
+;
+	mov ax,wait_thread_sel
+	mov ds,ax
+;
+	pop ds
+	ret
+init_wait_thread	ENDP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;	
+;
+;		NAME:			init
+;
+;		DESCRIPTION:	Init device-driver
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+init	PROC far
+	push ds
+	push es
+	pusha
+	mov bx,wait_code_sel
+	InitDevice
+;
+	mov eax,SIZE wait_thread_seg
+	mov bx,wait_thread_sel
+	AllocateFixedThreadMem
+;
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+	mov di,OFFSET init_wait_thread
+	HookCreateThread
+;
+	popa
+	pop es
+	pop ds
+	ret
+init	ENDP
+
+code	ENDS
+
+	END init
