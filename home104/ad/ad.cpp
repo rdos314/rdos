a816bbd1c335da85c0538a033e4a8da30ea84c6e
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2003-04-27 20:44:53 +0000

    no message

--- home104/ad/ad.cpp
+++ home104/ad/ad.cpp
@@ -6,9 +6,13 @@
 #include <time.h>
 
 #include "adcdev.h"
+#include "tempdev.h"
+#include "lightdev.h"
 #include "datetime.h"
 #include "secsamp.h"
 #include "minsamp.h"
+#include "hoursamp.h"
+#include "daysamp.h"
 #include "datetime.h"
 #include "bitdev.h"
 #include "videodev.h"
@@ -16,57 +20,257 @@
 #define FALSE	0
 #define TRUE	!FALSE
 
-static long double Meassured[8];
+long double lv;
+TSample lsmin, lsmax;
+
+long double tv[3];
+TSample tsmin[3], tsmax[3];
+
+long double rv[4];
+TSample rsmin[4], rsmax[4];
+
+void UpdateTime()
+{
+	TDateTime CurrentTime;
+	char str[80];
+
+	sprintf(str,
+			"%04d-%02d-%02d %02d.%02d.%02d",
+			CurrentTime.GetYear(),
+			CurrentTime.GetMonth(),
+			CurrentTime.GetDay(),
+			CurrentTime.GetHour(),
+			CurrentTime.GetMin(),
+			CurrentTime.GetSec());
+
+	RdosSetCursorPosition(15, 0);
+	RdosWriteString(str);
+}
+
+void UpdateLight(int all)
+{
+	char str[80];
+	TDateTime time;
+
+	if (all)
+		sprintf(str, "LJUS:  %7.3LfW/m2 (%7.3Lf, %7.3Lf)", lv, lsmin.GetMin(&time), lsmax.GetMax(&time));
+	else
+		sprintf(str, "LJUS:  %7.3LfW/m2", lv);
+
+	RdosSetCursorPosition(0, 0);
+	RdosWriteString(str);
+}
+
+void UpdateTemp(int index, int all)
+{
+	char par[20];
+	char str[80];
+	TDateTime time;
+
+	switch (index)
+	{
+		case 0:
+			strcpy(par, "TEMP:   ");
+			break;
+
+		case 1:
+			strcpy(par, "TANK:   ");
+			break;
+
+		case 2:
+			strcpy(par, "PANNA:  ");
+			break;
+	}
+
+	if (all)
+		sprintf(str, "%s %7.1LføC (%7.1Lf, %7.1Lf)", par, tv[index], tsmin[index].GetMin(&time), tsmax[index].GetMax(&time));
+	else
+		sprintf(str, "%s %7.1LføC", par, tv[index]);
+
+	switch (index)
+	{
+		case 0:
+			RdosSetCursorPosition(1, 0);
+			break;
+
+		case 1:
+			RdosSetCursorPosition(2, 0);
+			break;
+
+		case 2:
+			RdosSetCursorPosition(3, 0);
+			break;
+	}
+
+	RdosWriteString(str);
+}
+
+void UpdateRaw(int index, int all)
+{
+	char par[20];
+	char str[80];
+	TDateTime time;
+
+	switch (index)
+	{
+		case 0:
+			strcpy(par, "CH0:     ");
+			break;
+
+		case 1:
+			strcpy(par, "CH1:     ");
+			break;
+
+		case 2:
+			strcpy(par, "CH2:     ");
+			break;
+
+		case 3:
+			strcpy(par, "CH3:     ");
+			break;
+	}
+
+	if (all)
+		sprintf(str, "%s%7.3LfmV (%7.3Lf, %7.3Lf)", par, rv[index], rsmin[index].GetMin(&time), rsmax[index].GetMax(&time));
+	else
+		sprintf(str, "%s%7.3LfmV", par, rv[index]);
+
+	switch (index)
+	{
+		case 0:
+			RdosSetCursorPosition(4, 0);
+			break;
+
+		case 1:
+			RdosSetCursorPosition(5, 0);
+			break;
+
+		case 2:
+			RdosSetCursorPosition(6, 0);
+			break;
+
+		case 3:
+			RdosSetCursorPosition(7, 0);
+			break;
+	}
+
+	RdosWriteString(str);
+}
 
 void SecClear(TSample *sample)
 {
-	char str[80];
-	long double min, max, mean;
 	TDateTime time;
 
 	if (sample->GetCount())
 	{
-		min = sample->GetMin(&time);
-		max = sample->GetMax(&time);
-		mean = sample->GetMean(&time);
+		switch (sample->GetIndex())
+		{
+			case 7:
+				UpdateTime();
+				lv = sample->GetMean(&time);
+				UpdateLight(FALSE);
+				break;
 
-		sprintf(str, "%7.1LfmV", min);
-		RdosSetCursorPosition(1, 0);
-		RdosWriteString(str);
+			case 6:
+				tv[0] = sample->GetMean(&time);
+				UpdateTemp(0, FALSE);
+				break;
 
-		sprintf(str, "%7.1LfmV", max);
-		RdosSetCursorPosition(2, 0);
-		RdosWriteString(str);
+			case 5:
+				tv[1] = sample->GetMean(&time);
+				UpdateTemp(1, FALSE);
+				break;
 
-		sprintf(str, "%7.1LfmV", mean);
-		RdosSetCursorPosition(3, 0);
-		RdosWriteString(str);
+			case 4:
+				tv[2] = sample->GetMean(&time);
+				UpdateTemp(2, FALSE);
+				break;
+
+			case 3:
+				rv[0] = sample->GetMean(&time);
+				UpdateRaw(0, FALSE);
+				break;
+
+			case 2:
+				rv[1] = sample->GetMean(&time);
+				UpdateRaw(1, FALSE);
+				break;
+
+			case 1:
+				rv[2] = sample->GetMean(&time);
+				UpdateRaw(2, FALSE);
+				break;
+
+			case 0:
+				rv[3] = sample->GetMean(&time);
+				UpdateRaw(3, FALSE);
+				break;
+
+		}
 	}
 }
 
-void MinClear(TSample *sample)
+void RawProc(TSample *sample)
 {
 	char str[80];
-	long double min, max, mean;
+	long double val;
 	TDateTime time;
 
 	if (sample->GetCount())
 	{
-		min = sample->GetMin(&time);
-		max = sample->GetMax(&time);
-		mean = sample->GetMean(&time);
+		val = sample->GetMean(&time);
 
-		sprintf(str, "%7.1LfmV", min);
-		RdosSetCursorPosition(4, 0);
-		RdosWriteString(str);
+		switch (sample->GetIndex())
+		{
+			case 7:
+				lsmin.Add(&time, val);
+				lsmax.Add(&time, val);
+				UpdateLight(TRUE);
+				break;
 
-		sprintf(str, "%7.1LfmV", max);
-		RdosSetCursorPosition(5, 0);
-		RdosWriteString(str);
+			case 6:
+				tsmin[0].Add(&time, val);
+				tsmax[0].Add(&time, val);
+				UpdateTemp(0, TRUE);
+				break;
 
-		sprintf(str, "%7.1LfmV", mean);
-		RdosSetCursorPosition(6, 0);
-		RdosWriteString(str);
+			case 5:
+				tsmin[1].Add(&time, val);
+				tsmax[1].Add(&time, val);
+				UpdateTemp(1, TRUE);
+				break;
+
+			case 4:
+				tsmin[2].Add(&time, val);
+				tsmax[2].Add(&time, val);
+				UpdateTemp(2, TRUE);
+				break;
+
+			case 3:
+				rsmin[0].Add(&time, val);
+				rsmax[0].Add(&time, val);
+				UpdateRaw(0, TRUE);
+				break;
+
+			case 2:
+				rsmin[1].Add(&time, val);
+				rsmax[1].Add(&time, val);
+				UpdateRaw(1, TRUE);
+				break;
+
+			case 1:
+				rsmin[2].Add(&time, val);
+				rsmax[2].Add(&time, val);
+				UpdateRaw(2, TRUE);
+				break;
+
+			case 0:
+				rsmin[3].Add(&time, val);
+				rsmax[3].Add(&time, val);
+				UpdateRaw(3, TRUE);
+				break;
+
+		}
 	}
 }
 
@@ -80,147 +284,58 @@ void Sample(TAdcDevice *adc, TDateTime *time, long double value)
 	RdosWriteString(str);
 }
 
-void Sample()
+void CreateChannel(TAdcDevice *dev)
 {
-	long double val[16][100];
-	long double min, max;
-	long double mean;
-	int values;
-	int i;
-	int channel;
-	int value;
+	TSample *sec;
+	TSample *min;
+	TSample *hourmin, *hourmax;
+	TSample *day;
 
-	for (i = 0; i < 100; i++)
-	{
-		for (channel = 0; channel < 8; channel++)
-		{
-//			value = RdosReadAD(channel);
-			val[channel][i] = (long double)value / 32768.0 * 10000.0;
-		}
-		RdosWaitMilli(10);
-	}
+	dev->DefineInterval(1193000 / 20);
+	sec = new TSecSample(dev->GetChannel(), dev->GetUnit());
+	sec->ExcludeSmallest(3);
+	sec->ExcludeLargest(3);
+	sec->BeforeClear = SecClear;
+	dev->Define(sec);
 
-	for (channel = 0; channel < 8; channel++)
-	{
-		min = 10000.0;
-		max = -10000.0;
-
-		for (i = 0; i < 100; i++)
-		{
-			if (val[channel][i] < min)
-				min = val[channel][i];
-
-			if (val[channel][i] > max)
-				max = val[channel][i];
-		}
-
-		values = 100;
-		mean = 0.0;
-		for (i = 0; i < 100; i++)
-		{
-			if (val[channel][i] > min && val[channel][i] < max)
-				mean += val[channel][i];
-			else
-				values--;
-		}
-
-		if (values)
-			mean = mean / values;
-		else
-			mean = (min + max) / 2;
-		mean -= 1.0;
-
-		Meassured[channel] = mean;
-	}
-}
-
-void WriteRaw()
-{
-	int channel;
-	char str[20];
-
-	for (channel = 0; channel < 8; channel++)
-	{
-		sprintf(str, "%7.1LfmV", Meassured[channel]);
-		RdosSetCursorPosition(8 + channel, 0);
-		RdosWriteString(str);
-	}
-}
-
-
-void OldSample()
-{
-	int i;
-	int channel;
-	char str[20];
-	int x, y;
-	long double temp;
-	long double sumarr[8];
-	int countarr[8];
-	long double minarr[8];
-	long double maxarr[8];
-	TDateTime *time;
-	int PrevMin;
-	int PrevDay;
-    long double min, max;
-
-	for (channel = 0; channel < 8; channel++)
-	{
-		sumarr[channel] += Meassured[channel];
-		countarr[channel]++;
-	}
-
-	temp = sumarr[7] / countarr[7] / 50.0 / 100.0;
-	min = minarr[7] / 50.0 / 100.0;
-	max = maxarr[7] / 50.0 / 100.0;
-	sprintf(str, "LJUS:  %7.3LfW/m2 (%7.3Lf, %7.3Lf)", temp, min, max);
-	RdosSetCursorPosition(0, 0);
-	RdosWriteString(str);
-
-	temp = sumarr[6] / countarr[6] / 100.0;
-	min = minarr[6] / 100.0;
-	max = maxarr[6] / 100.0;
-	sprintf(str, "TEMP:  %7.2LfC (%7.2Lf, %7.2Lf)", temp, min, max);
-	RdosSetCursorPosition(1, 0);
-	RdosWriteString(str);
-
-	temp = Meassured[5] / 100.0;
-	min = minarr[5] / 100.0;
-	max = maxarr[5] / 100.0;
-	sprintf(str, "TANK:  %7.1LfC (%7.2Lf, %7.2Lf)", temp, min, max);
-	RdosSetCursorPosition(2, 0);
-	RdosWriteString(str);
-
-	temp = Meassured[4] / 100.0;
-	min = minarr[4] / 100.0;
-	max = maxarr[4] / 100.0;
-	sprintf(str, "PANNA: %7.1LfC (%7.2Lf, %7.2Lf)", temp, min, max);
-	RdosSetCursorPosition(3, 0);
-	RdosWriteString(str);
+	min = new TMinSample(dev->GetChannel(), dev->GetUnit());
+	sec->DefineMean(min);
+	min->BeforeClear = RawProc;
 }
 
 void cdecl main()
 {
 	int channel;
 	TWait Wait;
-	TAdcDevice *adc[8];
-	TSecSample *sec[8];
-	TMinSample *min[8];
+	TAdcDevice *adc;
+	TTempDevice *temp;
+	TLightDevice *light;
 
-	for (channel = 0; channel < 1; channel++)
-	{
-		adc[channel] = new TAdcDevice(&Wait, channel);
-		adc[channel]->DefineInterval(11930);
-		adc[channel]->OnSample = Sample;
-		sec[channel] = new TSecSample;
-		sec[channel]->ExcludeSmallest(5);
-		sec[channel]->ExcludeLargest(5);
-		sec[channel]->BeforeClear = SecClear;
-		adc[channel]->Define(sec[channel]);
-		min[channel] = new TMinSample;
-		min[channel]->BeforeClear = MinClear;
-		sec[channel]->Define(min[channel]);
-	}
+	RdosWaitMilli(250);
+
+	light = new TLightDevice(&Wait, 7);
+	CreateChannel(light);
+
+	temp = new TTempDevice(&Wait, 6);
+	CreateChannel(temp);
+
+	temp = new TTempDevice(&Wait, 5);
+	CreateChannel(temp);
+
+	temp = new TTempDevice(&Wait, 4);
+	CreateChannel(temp);
+
+	adc = new TAdcDevice(&Wait, 3);
+	CreateChannel(adc);
+
+	adc = new TAdcDevice(&Wait, 2);
+	CreateChannel(adc);
+
+	adc = new TAdcDevice(&Wait, 1);
+	CreateChannel(adc);
+
+	adc = new TAdcDevice(&Wait, 0);
+	CreateChannel(adc);
 
 	for (;;)
 		Wait.WaitForever();
