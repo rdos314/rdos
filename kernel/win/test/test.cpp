2118e5ff7d38f914c6e598b788ba036b270906f6
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2003-09-07 12:53:53 +0000

    *** empty log message ***

---
+++ kernel/win/test/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*##################  main  ###############
+*   Purpose....: main				            #
+*   In params..: *                                                          #
+*   Out params.: *                                                          #
+*   Returns....: *                                                          #
+*   Created....: 96-10-30 le                                                #
+*##########################################################################*/
+extern "C" void main(void)
+{
+	printf("hello\r\n");
+}
