e051b1204f3a0d9e8245d7de76b5d5ce9e7c5b79
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2003-12-03 11:06:08 +0000

    *** empty log message ***

--- kernel/zipsrc.bat
+++ kernel/zipsrc.bat
@@ -5,7 +5,7 @@ pkzip -P src.zip device\*.asm device\*.inc device\*.def device\makefile device\*
 pkzip -P src.zip win\*.asm win\*.inc win\*.def win\makefile win\*.bat win\*.zip win\*.dll win\*.h
 pkzip -P src.zip win32\*.asm win32\*.inc win32\*.def win32\makefile win32\*.bat win32\*.dll win32\*.h
 pkzip -P src.zip tools\*.asm tools\*.inc tools\*.def tools\makefile tools\*.cfg tools\*.bat tools\ndos.* tools\*.gft
-pkzip -P src.zip classlib\*.cpp classlib\*.h
-pkzip -P src.zip classlib\jpeg\*.cpp classlib\jpeg\*.h
-pkzip -P src.zip classlib\png\*.cpp classlib\png\*.h
-pkzip -P src.zip classlib\zlib\*.cpp classlib\zlib\*.h
+pkzip -P src.zip classlib\*.cpp classlib\*.h classlib\*.ide classlib\*.def
+pkzip -P src.zip classlib\jpeg\*.cpp classlib\jpeg\*.h classlib\jpeg\*.ide
+pkzip -P src.zip classlib\png\*.cpp classlib\png\*.h classlib\png\*.ide
+pkzip -P src.zip classlib\zlib\*.cpp classlib\zlib\*.h classlib\zlib\*.ide
