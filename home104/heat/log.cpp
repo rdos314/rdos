1623f879091b2950bbd59b521d117e99f88724a3
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2006-01-27 23:14:33 +0000

    *** empty log message ***

---
+++ home104/heat/log.cpp
@@ -0,0 +1,106 @@
+/*#######################################################################
+# RDOS operating system
+# Copyright (C) 1988-2006, Leif Ekblad
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version. The only exception to this rule
+# is for commercial usage in embedded systems. For information on
+# usage in commercial embedded systems, contact embedded@rdos.net
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# The author of this program may be contacted at leif@rdos.net
+#
+# log.h
+# Log class
+#
+########################################################################*/
+
+#include <string.h>
+
+#include "rdos.h"
+#include "log.h"
+#include "section.h"
+
+static TSection Section;
+
+/*##########################################################################
+#
+#   Name       : TLog::TLog
+#
+#   Purpose....: Log constructor
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TLog::TLog(const char *RootDir)
+{
+    strcpy(FRootDir, RootDir);
+    strlwr(FRootDir);
+
+    CreateRootDir();
+}
+
+/*##########################################################################
+#
+#   Name       : TLog::TLog
+#
+#   Purpose....: Log destructor
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TLog::~TLog()
+{
+}
+
+/*##########################################################################
+#
+#   Name       : TLog::DeviceName
+#
+#   Purpose....: Device name
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+void TLog::DeviceName(char *Name, int Size) const
+{
+	strcpy(Name, "LOG");
+}
+
+/*##########################################################################
+#
+#   Name       : TLog::CreateRootDir
+#
+#   Purpose....: Create root directory
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+void TLog::CreateRootDir()
+{
+    
+    FSection.Enter();
+
+    if (!RdosSetCurDir(FRootDir))
+        RdosMakeDir(FRootDir);
+        
+    FSection.Leave();
+}
