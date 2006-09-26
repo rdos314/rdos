5a460150b23938d3b6dbabdc012f26d6747661dc
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2006-09-26 14:23:37 +0000

    *** empty log message ***

---
+++ apps/freecom/capture.cpp
@@ -0,0 +1,117 @@
+/*#######################################################################
+# RDOS operating system
+# Copyright (C) 1988-2003, Leif Ekblad
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version. The only exception to this rule
+# is for commercial usage in embedded systems. For information on
+# usage in commercial embedded systems, contact embedded@rdos.net
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# The author of this program may be contacted at leif@rdos.net
+#
+# capture.cpp
+# Capture command class
+#
+########################################################################*/
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "rdos.h"
+#include "cmdhelp.h"
+#include "lang.h"
+#include "capture.h"
+
+#define FALSE 0
+#define TRUE !FALSE
+
+/*##########################################################################
+#
+#   Name       : TCaptureFactory::TCaptureFactory
+#
+#   Purpose....: Constructor for TCaptureFactory
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TCaptureFactory::TCaptureFactory()
+  : TCommandFactory("CAPTURE")
+{
+}
+
+/*##########################################################################
+#
+#   Name       : TCaptureFactory::Create
+#
+#   Purpose....: Create a command
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TCommand *TCaptureFactory::Create(TSession *session, const char *param)
+{
+	return new TCaptureCommand(session, param);
+}
+
+/*##########################################################################
+#
+#   Name       : TCaptureCommand::TCaptureCommand
+#
+#   Purpose....: Constructor for TCaptureCommand
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TCaptureCommand::TCaptureCommand(TSession *session, const char *param)
+  : TCommand(session, param)
+{
+	FHelpScreen.Load(TEXT_CMDHELP_CAPTURE);
+}
+
+/*##########################################################################
+#
+#   Name       : TCaptureCommand::Execute
+#
+#   Purpose....: Execute command
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+int TCaptureCommand::Execute(char *param)
+{
+	const char *str;
+
+	if (!ScanCmdLine(param, 0))
+		return 1;
+
+	if (FArgCount != 1)
+	{
+		 FMsg.Load(TEXT_ERROR_REQ_PARAM_MISSING);
+		 Write(FMsg.GetData());
+		 return E_Useage;
+	}
+
+	str = FArgList->FName.GetData();
+	return 0;
+}
+
