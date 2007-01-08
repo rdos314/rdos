449d1bb26de029a7bdd3d0bcced4a2042d9ea0cc
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2007-01-08 14:59:48 +0000

    *** empty log message ***

--- aspie-quiz/quiz9.cpp
+++ aspie-quiz/quiz9.cpp
@@ -1440,8 +1440,8 @@ void TQuiz9::ImportMvsp(const char *filename, int PcaType)
 			{
 				if (PcaType != PCA_TYPE_MIXED)
 				{
-//					if (PcaType == PCA_TYPE_FEMALE)
-//						d2 = -d2;
+					if (PcaType == PCA_TYPE_MALE)
+						d2 = -d2;
 
 					if (PcaType == PCA_TYPE_ALL)
 						d3 = -d3;
