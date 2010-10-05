fa10b7af518afd46526d8598e23440c64ba6b6ff
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2010-10-05 14:20:07 +0000

    Moved floppy to pcdev project.

--- kernel/pcdev/pcdev.mk
+++ kernel/pcdev/pcdev.mk
@@ -1,6 +1,6 @@
 project : c:\rdos\kernel\pcdev\pci.rdv c:\rdos\kernel\pcdev\rtc.rdv c:\rdos\&
 kernel\pcdev\ac97.rdv c:\rdos\kernel\pcdev\pic.rdv c:\rdos\kernel\pcdev\usb.&
-rdv c:\rdos\kernel\pcdev\ide.rdv .SYMBOLIC
+rdv c:\rdos\kernel\pcdev\ide.rdv c:\rdos\kernel\pcdev\floppy.rdv .SYMBOLIC
 
 !include c:\rdos\kernel\pcdev\pci.mk1
 !include c:\rdos\kernel\pcdev\rtc.mk1
@@ -8,3 +8,4 @@ rdv c:\rdos\kernel\pcdev\ide.rdv .SYMBOLIC
 !include c:\rdos\kernel\pcdev\pic.mk1
 !include c:\rdos\kernel\pcdev\usb.mk1
 !include c:\rdos\kernel\pcdev\ide.mk1
+!include c:\rdos\kernel\pcdev\floppy.mk1
