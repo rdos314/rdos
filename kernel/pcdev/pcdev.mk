0be8f723e9b379081240b43a4ee4c1d3afda544a
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2010-10-06 14:37:12 +0000

    Moved usb to usbdev project.

--- kernel/pcdev/pcdev.mk
+++ kernel/pcdev/pcdev.mk
@@ -1,13 +1,12 @@
 project : c:\rdos\kernel\pcdev\pci.rdv c:\rdos\kernel\pcdev\rtc.rdv c:\rdos\&
-kernel\pcdev\ac97.rdv c:\rdos\kernel\pcdev\pic.rdv c:\rdos\kernel\pcdev\usb.&
-rdv c:\rdos\kernel\pcdev\ide.rdv c:\rdos\kernel\pcdev\floppy.rdv c:\rdos\ker&
-nel\pcdev\stdcom.rdv c:\rdos\kernel\pcdev\vga.rdv .SYMBOLIC
+kernel\pcdev\ac97.rdv c:\rdos\kernel\pcdev\pic.rdv c:\rdos\kernel\pcdev\ide.&
+rdv c:\rdos\kernel\pcdev\floppy.rdv c:\rdos\kernel\pcdev\stdcom.rdv c:\rdos\&
+kernel\pcdev\vga.rdv .SYMBOLIC
 
 !include c:\rdos\kernel\pcdev\pci.mk1
 !include c:\rdos\kernel\pcdev\rtc.mk1
 !include c:\rdos\kernel\pcdev\ac97.mk1
 !include c:\rdos\kernel\pcdev\pic.mk1
-!include c:\rdos\kernel\pcdev\usb.mk1
 !include c:\rdos\kernel\pcdev\ide.mk1
 !include c:\rdos\kernel\pcdev\floppy.mk1
 !include c:\rdos\kernel\pcdev\stdcom.mk1
---
+++ kernel/usbdev/usbdev.mk
@@ -0,0 +1,3 @@
+project : c:\rdos\kernel\usbdev\usb.rdv .SYMBOLIC
+
+!include c:\rdos\kernel\usbdev\usb.mk1
