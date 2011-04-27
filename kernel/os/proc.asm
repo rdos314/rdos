7a525b089579eb88c6fc3031f020a411bccaabb2
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2011-04-27 08:46:14 +0000

    Added core selector to thread block.

--- kernel/os/proc.asm
+++ kernel/os/proc.asm
@@ -1058,6 +1058,8 @@ init_thread_block       PROC near
     mov es:p_process_sel,ax
     pop fs
 ;
+    mov ax,ds:p_core_sel
+    mov es:p_core_sel,ax
     mov ax,ds:p_app_sel
     mov es:p_app_sel,ax
     mov ax,ds:p_ldt_sel
@@ -2317,6 +2319,7 @@ init_first_process      Proc near
     mov ds:tss_es,fs
     GetProcessor
     mov fs:ps_null_thread,es
+    mov es:p_core_sel,fs
     ret
 init_first_process      Endp
 
