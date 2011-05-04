64cf7ee1a62b21d4fd073234909b3076308f9bda
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2011-05-04 15:06:36 +0000

    Moved netdev project sections to new interface.

--- kernel/netdev/rtl8xxx.asm
+++ kernel/netdev/rtl8xxx.asm
@@ -791,7 +791,7 @@ sPadOk:
     and dx,0FFFh
     or ax,dx
 ;
-    EnterSection ds:TxSection
+    EnterNewSection ds:TxSection
     or ecx,80000h
     push ecx
     push eax
@@ -843,7 +843,7 @@ ssNoPrev:
     inc bx
     and bx,3
     mov ds:TxBufPtr,bx
-    LeaveSection ds:TxSection
+    LeaveNewSection ds:TxSection
 ;
     pop edi
     pop dx
