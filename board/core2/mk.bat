ff0c596784699c61bbf3c885eb5dd9fa1508f9a2
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2011-11-18 22:07:08 +0000

    Update core2 BSP.

--- board/core2/mk.bat
+++ board/core2/mk.bat
@@ -1,6 +1,1 @@
-tdstrp32 \rdos\apps\freecom\command
-tdstrp32 \rdos\apps\ftpd\ftpd
-\rdos\apps\cfg2bin\win32\cfg2bin init
-\rdos\apps\cfg2bin\win32\cfg2bin safe
 \rdos\apps\cfg2bin\win32\cfg2bin rdos
-\rdos\apps\cfg2bin\win32\cfg2bin base
