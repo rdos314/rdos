3e077d48b9bd5be57a755081afce30360d4b6af7
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2011-11-18 21:14:57 +0000

    Updated freecom shell.

--- apps/freecom/zip.bat
+++
@@ -1,4 +0,0 @@
-del freecom.zip
-pkzip -P freecom.zip *.cpp *.h *.ide *.def *.exe 
-pkzip -P freecom.zip lib\*.cpp lib\*.h
-pkzip -P freecom.zip lang\*.cpp lang\*.h lang\*.ide lang\*.def lang\*.exe lang\*.dll lang\*.rc lang\*.lng
