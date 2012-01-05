1903cc81f90792d28b0b4313d40c0b3b2661d3c3
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2012-01-05 12:53:10 +0000

    ACPI now works on PPC.

--- kernel/acpi/acpi.c
+++ kernel/acpi/acpi.c
@@ -197,8 +197,6 @@ struct TDeviceEntry
     struct TResourceMemoryFixed32 *FixedMemory32ResourceList;
     struct TResourceAddress16 *Address16ResourceList;
     struct TResourceAddress32 *Address32ResourceList;
-    struct TResourceIrq *PossibleIrqResourceList;
-    struct TResourceExtendedIrq *PossibleExtendedIrqResourceList;
 
 };
 
@@ -260,6 +258,7 @@ void __far ImplTestGate(const char *msg)
     CurrState = ReadMsr(AMD8_PERF_STATUS);
 
     printf("%d", CurrState);
+
 }
 
 /*##########################################################################
@@ -1346,61 +1345,6 @@ void AddResource(struct TDeviceEntry *DevEntry, int size)
 
 /*##########################################################################
 #
-#   Name       : AddPossibleResource
-#
-#   Purpose....: Add a possible resource entry
-#
-#   In params..: *
-#   Out params.: *
-#   Returns....: *
-#
-##########################################################################*/
-void AddPossibleResource(struct TDeviceEntry *DevEntry, int size)
-{
-    int CopyLen;
-    int AllocLen;
-    char *ptr;
-    ACPI_RESOURCE *Resource;
-    struct TResourceIrq *IrqResource;
-    struct TResourceExtendedIrq *ExtendedIrqResource;
-    
-    ptr = &TempResourceBuf;
-    Resource = (ACPI_RESOURCE *)ptr;
-
-    while (size > 0)
-    {            
-        CopyLen = Resource->Length - 2 * sizeof(UINT32);
-        if (CopyLen > 0 && CopyLen < 0x1000)
-        {
-            AllocLen = sizeof(struct TResourceBase) + CopyLen; 
-            switch (Resource->Type)
-            {
-                case ACPI_RESOURCE_TYPE_IRQ:
-                    IrqResource = (struct TResourceIrq *)AcpiOsAllocate(AllocLen);
-                    memcpy(&IrqResource->Data, &Resource->Data, CopyLen);
-                    IrqResource->Next = DevEntry->PossibleIrqResourceList;
-                    DevEntry->PossibleIrqResourceList = IrqResource;
-                    break;
-                            
-                case ACPI_RESOURCE_TYPE_EXTENDED_IRQ:
-                    ExtendedIrqResource = (struct TResourceExtendedIrq *)AcpiOsAllocate(AllocLen);
-                    memcpy(&ExtendedIrqResource->Data, &Resource->Data, CopyLen);
-                    ExtendedIrqResource->Next = DevEntry->PossibleExtendedIrqResourceList;
-                    DevEntry->PossibleExtendedIrqResourceList = ExtendedIrqResource;
-                    break;
-                            
-                default:
-                    break;
-            }
-        }
-        size -= Resource->Length;
-        ptr += Resource->Length;
-        Resource = (ACPI_RESOURCE *)ptr;        
-    }
-}
-
-/*##########################################################################
-#
 #   Name       : GetHardware
 #
 #   Purpose....: Get hardware devices
@@ -1461,14 +1405,6 @@ void GetHardware()
                 if (Status == AE_OK)
                 {
                     AddResource(DevEntry, Buffer.Length);
-
-                    Buffer.Length = 0x4000;
-                    Buffer.Pointer = TempResourceBuf;
-
-                    Status = AcpiGetPossibleResources(DevEntry->Handle, &Buffer);
-
-                    if (Status == AE_OK)
-                        AddPossibleResource(DevEntry, Buffer.Length);
 
                     HardwareArr[HardwareCount] = DevEntry;
                     HardwareCount++;
