3e1caef613bb2ba135417c6cbd04cfd92ffa9446
rdos <rdos@a4d2bebb-172b-0410-bd86-fafe5f0301e3>
2015-10-22 08:27:16 +0000

    Added lon device.

---
+++ classlib/rdos/londev.cpp
@@ -0,0 +1,78 @@
+/*#######################################################################
+# RDOS operating system
+# Copyright (C) 1988-2002, Leif Ekblad
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version. The only exception to this rule
+# is for commercial usage in embedded systems. For information on
+# usage in commercial embedded systems, contact embedded@rdos.net
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# The author of this program may be contacted at leif@rdos.net
+#
+# londev.cpp
+# Lon device class
+#
+########################################################################*/
+
+#include "rdos.h"
+#include "londev.h"
+
+/*##########################################################################
+#
+#   Name       : TLonDevice::TLonDevice
+#
+#   Purpose....: Constructor for TLonDevice                                    
+#
+#   In params..: Height         requested font height
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TLonDevice::TLonDevice(int lonid)
+{
+    FLonId = lonid;
+    FLonHandle = 0;
+}
+
+/*##########################################################################
+#
+#   Name       : TLonDevice::~TLonDevice
+#
+#   Purpose....: Destructor for TLonDevice                                     
+#
+#   In params..: *
+#   Out params.: *
+#   Returns....: *
+#
+##########################################################################*/
+TLonDevice::~TLonDevice()
+{
+    if (FLonHandle)
+        RdosCloseLonModule(FLonHandle);
+}
+
+/*##########################################################################
+#
+#   Name       : TLonDevice::NotifyMsg
+#
+#   Purpose....: Notify message
+#
+#   In params..: msg        Lon message
+#   Out params.: size       Size of lon message
+#   Returns....: *
+#
+##########################################################################*/
+void TLonDevice::NotifyMsg(char *msg, int size)
+{
+}
