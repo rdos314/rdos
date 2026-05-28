975755952fcc8b33034c2300f228fcf5ca1bbe2e
Leif Ekblad <leif@rdos.net>
2026-05-28 16:48:50 +0200

    Fixed CDC for rdos.

--- classlib/rdos/serial.cpp
+++ classlib/rdos/serial.cpp
@@ -243,7 +243,7 @@ static void GetUsbSerial()
             info = InfoArr[i];
             if (!info)
             {
-                info = new TUsbSerialInfo(name.GetData(), 0, 0 , 0, 0);
+                info = new TUsbSerialInfo(name.GetData(), false, 0, 0 , 0, 0);
                 InfoArr[i] = info;
             }
         }
@@ -268,7 +268,7 @@ static void GetCdcSerial()
             info = InfoArr[i];
             if (!info)
             {
-                info = new TUsbSerialInfo(name.GetData(), 0, 0 , vendor, product);
+                info = new TUsbSerialInfo(name.GetData(), true, 0, 0 , vendor, product);
                 InfoArr[i] = info;
             }
         }
